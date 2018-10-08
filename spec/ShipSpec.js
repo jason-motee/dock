00015a29284fdee603e46d5652e563eb387d39dd
jason.motee <jason.motee@autotrader.co.uk>
2018-10-08 14:15:23 +0100

    Add setSail and dock methods to Ship

--- spec/ShipSpec.js
+++ spec/ShipSpec.js
@@ -2,15 +2,26 @@ describe ("Ship", function(){
 
     let port;
     let ship;
+    let arrivalPort;
 
     beforeEach (function(){
         port = new Port();
         ship = new Ship(port);
-
-
+        arrivalPort = new Port();
     })
 
-    it ("has a starting point", function(){
-        expect(ship.getCurrentPort()).toBe(port)
+    it ('has a starting point', function(){
+        expect(ship.getCurrentPort()).toBe(port);
     })
-})
+
+    it('can set sail from port', function(){
+        ship.setSail();
+        expect(ship.getCurrentPort()).toBeFalsy();
+    })
+
+    it('can dock at a port', function() {
+        ship.dock(arrivalPort);
+        expect(ship.getCurrentPort()).toBe(arrivalPort);
+    })
+
+});
--- src/Ship.js
+++ src/Ship.js
@@ -1,5 +1,6 @@
 function Ship (port){
-    this.Port = port;
+    this._currentPort = port;
+    
 }
 
 Ship.prototype.getShip = function (){
@@ -8,5 +9,13 @@ return Ship;
 }
 
 Ship.prototype.getCurrentPort = function (){
-    return this.Port;
+    return this._currentPort;
+}
+
+Ship.prototype.setSail = function() {
+    this._currentPort = null;
+}
+
+Ship.prototype.dock = function(arrivalPort) {
+    this._currentPort = arrivalPort;
 }
